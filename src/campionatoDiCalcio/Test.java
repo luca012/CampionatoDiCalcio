1702ad2db1c6cc4a2bef6ecf42661b8e5c7be69e
luca <68597426+asdrubale0@users.noreply.github.com>
2022-04-08 10:39:26 +0200

    Aggiunta classe input

--- src/campionatoDiCalcio/Test.java
+++ src/campionatoDiCalcio/Test.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Test {
-
-	private static Scanner input = new Scanner(System.in);
-
-	public static int menu() {
-		int scelta;
+	
+	public static void menu() {
 		System.out.println("\n *** Gestione CSV Campionato di calcio *** ");
 		System.out.println("1. Stampa classifica");
 		System.out.println("2. Visualizza partite di una squadra");
@@ -18,29 +15,27 @@ public class Test {
 		System.out.println("4. Esporta classifica");
 		System.out.println("0. Fine programma");
 		System.out.print("Opzione scelta: ");
-		scelta = input.nextInt();
-		input.nextLine();
-		return scelta;
+		//input.nextLine();
 	}
 
 	public static void main(String[] args) {
 		Campionato c = new Campionato("Premier League", "2019-2020");
 		c.carica_dati("league2020.csv");
+		Input input = new Input();
 		int scelta;
 		do {
-			scelta = menu();
+			menu();
+			scelta = input.inputInt("");
 			switch (scelta) {
 			case 1:
 				c.stampaClassifica();
 				break;
 			case 2:
-				System.out.print("\nInserire il nome di una squadra: ");
-				String nomeSquadra = input.nextLine();
+				String nomeSquadra = input.inputString("\nInserire il nome di una squadra: ");
 				c.stampaPartiteSquadra(nomeSquadra);
 				break;
 			case 3:
-				System.out.print("\nInserire il numero di una giornata: ");
-				int numGiornata = input.nextInt();
+				int numGiornata = input.inputInt("\nInserire il numero della giornata (1-38): ");
 				c.stampaPartiteGiornata(numGiornata);
 				break;
 			case 4:
